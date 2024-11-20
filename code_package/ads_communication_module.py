8784eeaf48b1741cc7d758774a4a42ad01022947
sanderthibau <158479434+sanderthibau@users.noreply.github.com>
2024-11-20 15:33:08 +0100

    test search index functions

--- code_package/ads_communication_module.py
+++ code_package/ads_communication_module.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pyads
 import time
 
@@ -37,33 +38,66 @@ def read_twincat_structure(plc, struct_name='Global.Buffer',
     ordered_dictionary = plc.read_structure_by_name(struct_name, struct_def)
     return ordered_dictionary
 
-def search_index_nextStep(data_counter, previous_counter):
+def search_index_nextStep(data_counter, previous_counter, max_missing=1):
     """
     When a circular buffer is read out, this function searches the data point following on the last processed point.
     """
+    if not isinstance(data_counter, np.ndarray):
+        data_counter = np.array(data_counter)
     try:
-        index = data_counter.index(previous_counter+1)
+        index = np.where(data_counter==previous_counter+1)[0][0]
     except:
         try:
-            index = data_counter.index(previous_counter+2)
+            index = np.where((data_counter<previous_counter+2+max_missing)&(data_counter>previous_counter))[0][0]
         except:
             index = 'NoIndexFound'
-            print('More than 1 data point is missing, the digital twin is of track')
+            print('More than 1 data point is missing, the digital twin is off track')
+        
     return index
+def search_index_lastStep(data_counter):
+    """
+    The last data point added to the buffer has the highest value of Counter
+    """
+    if not isinstance(data_counter, np.ndarray):
+        data_counter = np.array(data_counter)
+    index = np.argmax(data_counter)
+    return index
+
+def put_array_chronologically(data_array, index_first, index_last):
+    if not isinstance(data_array, np.ndarray):
+        data_array = np.array(data_array)
+
+    if index_last<index_first:
+        sorted = np.concatenate((data_array[index_first:],data_array[:index_last+1]))
+    else:
+        sorted = data_array[index_first:index_last+1]
+    return sorted
 
 def select_useful_data(buffer_od, previous_counter):
     sorted_od = buffer_od
     return sorted_od
 
 if __name__ == "__main__":
-    plc = pyads.Connection(AMSNETID, pyads.PORT_TC3PLC1)
-    plc.open()
-    
+    print('running')
 
-    data = read_twincat_structure(plc)
-    
-    
-    plc.close()
+
+    testing = 1
+    if testing == 1:
+        plc = pyads.Connection(AMSNETID, pyads.PORT_TC3PLC1)
+        plc.open()
+        buffer_od = read_twincat_structure(plc)
+        plc.close()
+       #print(buffer)
+        last_counter = 8
+        array_of_counters = np.array(buffer_od['aDataCounter'])
+        starting_index = search_index_nextStep(array_of_counters,last_counter)
+        print(starting_index)
+        
+
+
+
+    """
+    HISTOGRAM OF TIME TO READ BUFFER (100x[counter,time,torque,angle])
     x = []
     tlist = []
 
@@ -88,4 +122,4 @@ if __name__ == "__main__":
     plt.xlabel('reading time')
     plt.ylabel('amount')
     plt.show()
-    
+    """
--- code_package/testing.py
+++ code_package/testing.py
@@ -1,12 +1,28 @@
 import pytest
 from cartesian_robot_module import forced_response
-from ads_communication_module import search_index_nextStep, select_useful_data
+from ads_communication_module import search_index_nextStep, search_index_lastStep, put_array_chronologically, select_useful_data
+import numpy as np
 
-@pytest.mark.parametrize("arrayOfCounters,lastCounter,nextIndex", [([6,7,8,2,3,4,5], 1, 3), ([6,7,8,2,3,4,5], 2, 4), ([6,7,8,2,3,4,5], 0, 3), ([6,7,8,3,4,5], 0, 'NoIndexFound')])
-                         
-def test_search_index_nextStep(arrayOfCounters, lastCounter, nextIndex):
-    assert search_index_nextStep(arrayOfCounters, lastCounter) == nextIndex
 
+
+#@pytest.mark.parametrize("lastCounter,nextIndex", [(1, 3), (2, 4), (0, 3), (-1, 'NoIndexFound')])
+a = [6,7,8,2,3,4,5]                     
+def test_search_index_nextStep():
+    
+    assert search_index_nextStep(a, 1) == 3
+    assert search_index_nextStep(a, 2) == 4
+    assert search_index_nextStep(a, 0) == 3
+    assert search_index_nextStep(a, -1) == 'NoIndexFound'
+    missing = 2
+    assert search_index_nextStep(a, -1, missing) == 3
+
+
+def test_search_index_lastStep():
+    assert search_index_lastStep(a) == 2
+
+@pytest.mark.parametrize("firstI,lastI,sorted", [(3,2,[2,3,4,5,6,7,8]), (4,6,[3,4,5]), (2,2,[8])])
+def test_put_array_chronologically(firstI,lastI,sorted):
+    assert np.all(put_array_chronologically(a, firstI, lastI)) == np.all(sorted)
 
 
 
