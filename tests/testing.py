6572b0f4d0a39d97d0a9ec3801e991eb36a64ef4
sanderthibau <158479434+sanderthibau@users.noreply.github.com>
2025-01-09 16:51:38 +0100

    testing in correct package folder

--- tests/testing.py
+++ tests/testing.py
@@ -1,7 +1,7 @@
 import pytest
-from cartesian_robot_module import forced_response
-from ads_communication_module import search_index_nextStep, search_index_lastStep, put_array_chronologically, select_useful_data
-from csv_plot_module import create_dict_HeadersAndData
+from code_package.cartesian_robot_module import forced_response
+from code_package.ads_communication_module import search_index_nextStep, search_index_lastStep, put_array_chronologically, select_useful_data
+from code_package.csv_plot_module import create_dict_HeadersAndData
 import numpy as np
 from collections import OrderedDict
 import time
